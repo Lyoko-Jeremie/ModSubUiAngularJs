288340f02c5393bc709a3cd74229fa34abdf853e
LyokoJeremie <lucheng989898@gmail.com>
2024-07-28 17:25:57 +0800

    .

--- src_inject/test.ts
+++ src_inject/test.ts
@@ -1,4 +1,9 @@
 import {ModSubUiAngularJsBody} from "./ModSubUiAngularJsBody";
+import {createOrderComponent} from "./AngularJs/Componnet/OrderComponent";
 
 // @ts-ignore
 window.modModSubUiAngularJsBody = new ModSubUiAngularJsBody();
+
+
+// @ts-ignore
+window.modModSubUiAngularJsBody.appContainerManager.ModGuiConfig.registryComponent(createOrderComponent);
